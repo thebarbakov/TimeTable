662bd61c29b47e9687a1d897b83ecc23f4e25426
thebarbakov <94574341+thebarbakov@users.noreply.github.com>
2022-02-16 01:35:25 +0300

    first

--- scripts/popup.js
+++ scripts/popup.js
@@ -7,13 +7,6 @@ const popupTeacher = popup.querySelector('#popup-teacher')
 const popupButtonClose = popup.querySelector('.popup__close')
 const popupOverlay = popup.querySelector('.popup__overlay')
 
-const scrollToTop = () => window.scrollTo(scrollX, 0, );
-
-function blockScrolling(){
-    scrollToTop();
-    window.addEventListener('scroll', scrollToTop);
-};
-
 function openPopup(title, type, room, teacher, num){
     popupTitle.textContent = title
     popupTime.textContent = timeTable[num-1].timePeriod
@@ -24,11 +17,9 @@ function openPopup(title, type, room, teacher, num){
     popupOverlay.addEventListener('click', evt => popupClose())
     popup.classList.add('animation__open')
     popup.classList.add('popup_active')
-    blockScrolling();
 }
 
 function popupClose() {
-    window.removeEventListener('scroll', scrollToTop);
     popup.classList.remove('animation__open');
     popup.classList.add('animation__close');
     function removeClasses() {
