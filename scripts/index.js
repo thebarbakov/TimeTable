c3f0dc478e681e189733fcb857f2431fa0f55617
thebarbakov <94574341+thebarbakov@users.noreply.github.com>
2022-02-16 03:50:58 +0300

    first

--- scripts/index.js
+++ scripts/index.js
@@ -155,9 +155,9 @@ function createLessonCard(title, type, room, teacher, num, date) {
         newLessonCard.querySelector('.lesson__ticket-des').classList.add('lesson__ticket-des_active')
     })
     newLessonCard.querySelector('.lesson__ticket').addEventListener('touchend', event => {
-        setTimeout(timeHandlerTickets, 2000)
         const timeHandlerTickets = () => newLessonCard.querySelector('.lesson__ticket-des').classList.remove
-        ('lesson__ticket-des_active')
+        ('lesson__ticket-des_active');
+        setTimeout(timeHandlerTickets, 2000);
     })
     newLessonCard.querySelector('.lesson__title').addEventListener('click', evt => 
     openPopup(title, type, room, teacher, num))
